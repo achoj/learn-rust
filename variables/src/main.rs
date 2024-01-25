2b2ff8de89807bfd0c219b82c87ddcbfabd45af7
goodfanqie <357017287@qq.com>
2024-01-25 13:26:35 +0800

    add .gitignore

---
+++ variables/src/main.rs
@@ -0,0 +1,21 @@
+
+fn main() {
+    println!("---Learn mut variables---");
+    let x = 10;
+    println!("The value of x is {x}");
+
+    let x = 100;
+    println!("The value of x is {x}");
+
+    println!();
+    println!("---Learn shadowing---");
+    let y = 5;
+    let y = y + 1;
+
+    {
+        let y = y * 2;
+        println!("The value of y in the inner scope is : {y}");   
+    }
+
+    println!("The value of y is: {y}");
+}
